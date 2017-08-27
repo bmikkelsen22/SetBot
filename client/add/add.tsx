f69a1cb40691024815fe03275771eb9589e54fa7
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-08-27 14:17:17 -0700

    working on add card section

---
+++ client/add/add.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import * as Picker from './picker';
+import { Color, Card, Shade, Shape } from '../card/model';
+
+interface State {
+    color: Color;
+    count: number;
+    shade: Shade;
+    shape: Shape;
+}
+
+interface NameValue {
+    name: string;
+    value: any;
+}
+
+function nv(name: string, value: any): NameValue {
+    return {
+        name: name,
+        value: value
+    }
+}
+
+export class Component extends React.Component<undefined, State> {
+    options = {
+        colors: [
+            nv('Green', Color.green), 
+            nv('Red', Color.red), 
+            nv('Purple', Color.purple)
+        ],
+        counts: [
+            nv('One', 1), 
+            nv('Two', 2),
+            nv('Three', 3)
+        ],
+        shades: [
+            nv('Empty', Shade.empty), 
+            nv('Shaded', Shade.shaded), 
+            nv('Full', Shade.full)
+        ],
+        shapes: [
+            nv('Diamond', Shape.diamond), 
+            nv('Oval', Shape.oval), 
+            nv('Squiggle', Shape.squiggle)
+        ]
+    }
+
+    constructor() {
+        super();
+        this.state = {
+            color: this.options.colors[0].value as Color,
+            count: this.options.counts[0].value as number,
+            shade: this.options.shades[0].value as Shade,
+            shape: this.options.shapes[0].value as Shape
+        }
+    }
+
+    onSelectColor = (index: number) => {
+        this.setState({
+            color: this.options.colors[index].value as Color
+        });
+    }
+
+    render() {
+        return (
+            <div className='add-container'>
+                <Picker.Component 
+                    header='Color'
+                    options={this.options.colors.map(c => c.name)}
+                    onSelect={this.onSelectColor} />
+            </div>
+        );
+    }
+}
---
+++ client/add/picker.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { Color, CardExtensions } from '../card/model';
+
+interface Props {
+    header: string;
+    options: string[];
+    onSelect: (index: number) => void;
+}
+
+interface State {
+    selected: number;
+}
+
+export class Component extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            selected: 0
+        };
+    }
+
+    onOptionClick(index) {
+        const newState = {
+            selected: index
+        }
+        this.setState(newState);
+    }
+
+    renderOption = (index: number) => {
+        let cls = 'options';
+        if (this.state.selected === index) {
+            cls += ' selected-option';
+        }
+        return (
+            <div className={cls} key={index} onClick={() => this.onOptionClick(index)}>
+                {this.props.options[index]}
+            </div>
+        );
+    }
+
+    render() {
+        let optionsJsx = this.props.options.map((o, i) => this.renderOption(i))
+        return (
+            <div className='picker'>
+                <div className='picker-header'>
+                    {this.props.header}
+                </div>
+            </div>
+        );
+    }
+}
+
