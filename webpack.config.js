ed68a6dd31ea7a745fe9e1b3565ebd795bef4879
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-11-30 23:13:42 -0800

    move public folder to root directory

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: "./page",
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname, "../dist/public/js")
+        path: path.resolve(__dirname, "./public/js")
     },
 
     // Enable sourcemaps for debugging webpack's output.
